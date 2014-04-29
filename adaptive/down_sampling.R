e0cc827943074c6e6c6c306ecaca02b26491f58e
Joshua Shen <joshuashen@gmail.com>
2014-04-29 09:53:12 -0400

    calculate JSD in each simulation; add noise to read number

--- adaptive/down_sampling.R
+++ adaptive/down_sampling.R
@@ -1,73 +1,66 @@
 # downsample adaptive data
 
-downsample <- function(tcr, target = 2000000, f = 0.5) {
+downsample <- function(tcr, target = 2000000, tnoise = 0.125,  nsim = 10) {
 	# target is the number of targeted reads per sample
-#	total = sum(tcr$total)
+	# noise is \sigma / \mu for read number
+        # nsim: number of simulations
+
 	nc = ncol(tcr)
 	nsample = nc - 2
 	nclone = nrow(tcr)
 	
-#	ratio = target / total * nsample
-#	cat("Down-sampling to", ratio, sep="\t")
-	
+	results = rep(0, nsim + 1)
+	results[1] = jensen_shannon(tcr[1:1000,3], tcr[1:1000,4])
 
-	treads = rbinom(1, sum(tcr$total), ratio)
-	readA = rmultinom(1, treads, c(f, 1-f))
-	      
-	# then multinomial to get frequency of each clone
-	## rmultinom too slow!
+	nreads = rnorm(nsim * 2 , target, tnoise * target)
+	p = matrix(ncol=2, nrow=nclone)
 
-	for (j in 1:2) {
-	    lambda = target * tcr[,j+2] / sum(tcr[,j+2])
-	    tcr[,j+2] = rpois(nclone, lambda)
+	for (i in 1:nsim) {
+		for (j in 1:2) {
+		    
+		    lambda = nreads[(i-1)*2+j] * tcr[,j+2] / sum(tcr[,j+2])
+	    	    p[,j] = rpois(nclone, lambda) 
+		}
+
+		q = p[p[,1] > 1 | p[,2]>1, ] 
+		results[1+i] = jensen_shannon(q[1:1000,1], q[1:1000,2])		
+		
 	}
+#	tcr[,2] = apply(tcr[,3:4], 1, sum)
 
-	for (i in 1:nclone) {
-	    tcr[i,2] = sum(tcr[i, 3:nc]) 
-	}
+	cat(results, "\n", sep="\t")
 
-	# normalize
-	for (j in 3:nc) {
-		tcr[,j] = tcr[,j] / sum(tcr[,j]) * 100
-	}
-	return(tcr)
+#	return(tcr[tcr$total >=2, ])
 
 }
 
 
 
-
-
-## re-implement multinorm random number function
-
-rmultinomial <- function(n, p){
- ncat <- length(p)
-#     Check Arguments
-      if (n < 0) {cat("n < 0 ","\n"); break}
-      if (ncat <= 1) {cat("ncat <= 1 ","\n"); break}
-      if (any(p < 0.0)) {cat("Some P(i) < 0 ","\n"); break}
-      p = p / sum(p)
-      if (any(p > 1.0)) {cat("Some P(i) > 1.0 ","\n"); break}
- eps <- .Machine$double.eps^0.9
-      if (sum(p) > (1.0 + eps) | sum(p) < (1.0 - eps) ) {cat("Sum of P(i)
-should equal 1.0 ","\n"); break}
-
-#     Initialize variables
-      ntot <- n
-      sum <- 1.0
-      ix <- rep(0,ncat)
-
-#     Generate the observation
-      for (icat in 1:(ncat - 1)) {
-          prob <-  p[icat]/sum
-          ix[icat] <-  rbinom(1,ntot,prob)
-          ntot <- ntot - ix[icat]
-          if (ntot <= 0) return(ix)
-          sum <- sum - p[icat]
- }
-      ix[ncat] <- ntot
- return (ix)
+shannon.entropy <- function(p)
+{
+	if (min(p) < 0 || sum(p) <= 0)
+	   return(NA)
+	   p.norm <- p[p>0]/sum(p)
+	   -sum(log2(p.norm)*p.norm)
 }
+
+
+jensen_shannon <- function(p, q){
+	       ## JSD = H(0.5 *(p +q)) - 0.5H(p) - 0.5H(q)
+ 	       # H(X) = \sum x_i * log2(x_i)
+#	       p = p[p >0 & q >0]
+#	       q = q[p>0 & q>0]
+	       p = p / sum(p)
+	       q = q / sum(q)
+	       Hj = shannon.entropy(0.5 *(p+q)) 
+	       Hp = shannon.entropy(p) 
+	       Hq = shannon.entropy(q)
+	       
+	       jsd = Hj - 0.5*(Hp+Hq)
+#	       cat(Hj, Hp, Hq, jsd, "\n")
+	       return(jsd)
+}
+
 
 # args<-commandArgs(TRUE)
 library(getopt)
@@ -75,7 +68,8 @@ library(getopt)
 spec <- matrix(c(
         'input'     , 'i', 1, "character", "input csv or tsv file (required)",
         'type'     , 't', 2, "character", "csv or tsv (optional); default: tsv",
-        "fraction" , "f", 2, "double", "[0.5] fraction of reads for T1",
+        "noise" , "s", 2, "double", "[0.125] ratio of stddev to mean of reads number",
+	"nreads", "n", 2, "double", "[2000000] mean number of reads per sample in each simulation ",
         'help'   , 'h', 0, "logical",   "this help"
 ),ncol=5,byrow=T)
 
@@ -95,11 +89,16 @@ if (!is.null(opt$type)) {
    
 }
 
-frac = 0.5
-
-if (!is.null(opt$fraction)) {
-   frac = opt$fraction
+noise = 0.125
+nreads = 2000000
+if (!is.null(opt$noise)) {
+   noise = opt$noise
 }
+
+if(!is.null(opt$nreads)) {
+   nreads = opt$nreads			
+}
+
 
 if( itype == "c")  {
     tcr = read.table(file, header=T, sep=",")
@@ -109,10 +108,10 @@ if( itype == "c")  {
   tcr = read.table(file, header=T)			
 }
 
-tcr=downsample(tcr)
+downsample(tcr, nreads , noise)
 
 
-write.table(tcr, "", quote=F, sep="\t", row.names = F)
+## write.table(tcr, "", quote=F, sep="\t", row.names = F)
 
 
 
